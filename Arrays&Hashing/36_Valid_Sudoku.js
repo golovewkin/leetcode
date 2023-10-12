e14e86ae7230912992164f92820511610d7aba33
golovewkin <golovewkin@gmail.com>
2023-10-12 17:02:51 -0600

    isValidSudoku wip

---
+++ Arrays&Hashing/36_Valid_Sudoku.js
@@ -0,0 +1,16 @@
+// 36. Valid Sudoku
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+//
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+export const isValidSudoku = function (board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {}
+  }
+};
