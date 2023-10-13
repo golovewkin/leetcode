8fcaca87103c9b35f1a15bef1421018b9ee845be
golovewkin <golovewkin@gmail.com>
2023-10-12 21:51:21 -0600

    jest works

--- Arrays&Hashing/__tests__/1_Two_Sum.testjs.js
+++ Arrays&Hashing/__tests__/1_Two_Sum.testjs.js
@@ -3,6 +3,6 @@ import { describe, expect, test } from "@jest/globals";
 
 describe("twoSum should work", () => {
   test("checks the base case", () => {
-    expect(two_Sum([2, 7, 11, 15], 9)).toBe([1, 0]);
+    expect(two_Sum([2, 7, 11, 15], 9)).toStrictEqual([1, 0]);
   });
 });
