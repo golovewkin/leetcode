30888e52a3ee1155a521545ba02432f9a8220951
golovewkin <golovewkin@gmail.com>
2023-10-12 19:00:52 -0600

    217. Contains Duplicate

---
+++ 242. Valid Anagram.js
@@ -0,0 +1,25 @@
+// 242. Valid Anagram
+// Easy
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+//
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+//
+// Input: s = "rat", t = "car"
+// Output: false
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = function(s, t) {
+    return s.split('').sort().toString() === t.split('').sort().toString();
+};
+
+console.log(isAnagram('anagram', 'nagaram'));
