af1c83bd15b9fbf1a89f3dd8c2c9948d231e14f7
golovewkin <golovewkin@gmail.com>
2023-10-25 20:24:06 -0600

    threeSum preparations add

---
+++ Two_Pointers/__tests__/15_3Sum.test.js
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "@jest/globals";
+import { threeSum } from "../15_3Sum";
+
+describe("threeSum should work properly", () => {
+  test("checks the base case", () => {
+    expect(threeSum([-1, 0, 1, 2, -1, -4])).toStrictEqual([
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ]);
+  });
+
+  test("checks the second case with eno solution", () => {
+    expect(threeSum([0, 1, 1])).toStrictEqual([]);
+  });
+
+  test("checks the third case with null in array", () => {
+    expect(threeSum([0, 0, 0])).toStrictEqual([[0, 0, 0]]);
+  });
+});
