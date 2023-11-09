316e2255ac95b645784cb03aa7510783a56524d0
golovewkin <golovewkin@gmail.com>
2023-11-08 20:40:22 -0700

    add generateParenthesis solution

--- Stack/22_Generate_Parentheses.js
+++ Stack/22_Generate_Parentheses.js
@@ -10,4 +10,15 @@
 // Input: n = 1
 // Output: ["()"]
 
-export const generateParenthesis = function (n) {};
+export const generateParenthesis = function (n) {
+  const res = [];
+  pair("(", n - 1, n);
+  return res;
+
+  function pair(current, left, right) {
+    if (left === 0 && right === 0) return res.push(current);
+    if (left > right) return;
+    if (left > 0) pair(current + "(", left - 1, right);
+    if (right > 0) pair(current + ")", left, right - 1);
+  }
+};
