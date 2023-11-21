dc02786a54e0b198636d3ba24aa540c00b74e0ac
golovewkin <golovewkin@gmail.com>
2023-11-20 21:13:04 -0700

    binarySearch add exsersise

---
+++ Binary_Search/__tests__/704_Binary_Search.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "@jest/globals";
+import { binarySearch } from "../704_Binary_Search";
+
+describe("MinStack should work properly", () => {
+  test("checks the base case", () => {
+    expect(binarySearch([-1, 0, 3, 5, 9, 12], 9)).toEqual(4);
+  });
+
+  test("checks the 2nd case", () => {
+    expect(binarySearch([-1, 0, 3, 5, 9, 12], 2)).toEqual(-1);
+  });
+});
